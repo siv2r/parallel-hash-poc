d60f93200970ce04548748a627d5f25fd304ace3
siv2r <siv2ram@gmail.com>
2024-03-18 13:42:27 +0530

    remove shake128 & sha3_256 from main()
    Also, add useful code comments

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,17 @@
 // use hex_literal::hex;
-use sha3::{CShake128, CShake128Core, Shake128};
+use sha3::{CShake128, CShake128Core};
 use digest::{Update, ExtendableOutput, XofReader};
 
 fn main() {
     let mut result = [0u8; 32];
-    let mut hasher1 = Shake128::default();
-    hasher1.update(b"abc");
-    let mut reader1 = hasher1.finalize_xof();
-    reader1.read(&mut result);
-    println!("shake output = {}", hex::encode(result));
+    let mut hasher = ParallelHash::new(5);
+    hasher.update(b"msg to hash");
+    hasher.finalize(&mut result);
+    println!("Hash Digest: {}", hex::encode(result));
 
-    let mut hasher2 = CShake128::from_core(CShake128Core::new(b"def"));
-    hasher2.update(b"abc");
-    let mut reader2 = hasher2.finalize_xof();
-    reader2.read(&mut result);
-    println!("cshake output = {}", hex::encode(result));
-
-    let mut b1: [u8; 9] = [0u8; 9];
-    let mut b2: [u8; 9] = [0u8; 9];
-    let lencode = left_encode(450000, &mut b1);
-    let rencode = right_encode(450000, &mut b2);
-    println!("left encoding = {}", hex::encode(lencode));
-    println!("left encoding = {}", hex::encode(rencode));
 }
 
+/// Coverts integer to a big endian byte-array with its length as first byte
 fn left_encode(val: u64, b: &mut [u8; 9]) -> &[u8] {
     b[1..].copy_from_slice(&val.to_be_bytes());
     let i = b[1..8].iter().take_while(|&&a| a == 0).count();
@@ -31,6 +19,7 @@ fn left_encode(val: u64, b: &mut [u8; 9]) -> &[u8] {
     &b[i..]
 }
 
+/// Coverts integer to a big endian byte-array with its length as last byte
 fn right_encode(val: u64, b: &mut [u8; 9]) -> &[u8] {
     b[..8].copy_from_slice(&val.to_be_bytes());
     let i = b[0..8].iter().take_while(|&&a| a == 0).count();
@@ -38,11 +27,18 @@ fn right_encode(val: u64, b: &mut [u8; 9]) -> &[u8] {
     &b[i..]
 }
 
+/// Stores a partial-block whose length is less than `block_size``
 pub struct IncompleteState {
     state: CShake128,
     absorbed: usize,
 }
 
+/// The `ParallelHash` hash functions defined in [`SP800-185`].
+///
+/// The purpose of `ParallelHash` is to support the efficient hashing of very long strings, by
+/// taking advantage of the parallelism available in modern processors. This version supports
+/// only [`128-bit`] security strength.
+/// [`SP800-185`]: https://nvlpubs.nist.gov/nistpubs/SpecialPublications/NIST.SP.800-185.pdf
 pub struct ParallelHash {
     state: CShake128,
     blocks: usize,
@@ -52,7 +48,11 @@ pub struct ParallelHash {
 
 impl ParallelHash {
     /// Creates an instance of the ParallelHash
-    fn new(custom_string: &[u8], block_size: usize) -> ParallelHash {
+    fn new(block_size: usize) -> ParallelHash {
+        Self::new_with_custom_string(&[], block_size)
+    }
+
+    fn new_with_custom_string(custom_string: &[u8], block_size: usize) -> ParallelHash {
         let mut state = CShake128::from_core(CShake128Core::new_with_function_name(b"ParallelHash", custom_string));
         let mut enc_block_size = [0u8; 9];
         state.update(left_encode(block_size as u64, &mut enc_block_size));
@@ -75,6 +75,8 @@ pub trait Hasher {
 
 impl Hasher for ParallelHash {
     fn update(&mut self, mut input: &[u8]) {
+        // if previous input block was incomplete, combine it with upcoming input
+        // to create a complete block with length `block_size`
         if let Some(mut partial) = self.has_partial_block.take() {
             let to_absorb = self.block_size - partial.absorbed;
             if input.len() >= to_absorb {
@@ -93,6 +95,8 @@ impl Hasher for ParallelHash {
             }
         }
 
+        // break the given input into complete-blocks and pass them to cSHAKE128
+        // to create a new input
         let input_blocks_cnt = input.len() / self.block_size * self.block_size;
         let input_blocks = &input[..input_blocks_cnt];
         let input_end = &input[input_blocks_cnt..];
@@ -106,11 +110,13 @@ impl Hasher for ParallelHash {
             temp_out
         });
 
+        // hash the new input with cSHAKE128
         for part in parts {
             self.state.update(&part);
             self.blocks += 1;
         }
 
+        // if there exists a partial input block, store it in `IncompleteState`
         if !input_end.is_empty() {
             assert!(self.has_partial_block.is_none());
             let mut state = CShake128::from_core(CShake128Core::new(&[]));
@@ -123,6 +129,7 @@ impl Hasher for ParallelHash {
     }
 
     fn finalize(mut self, output: &mut [u8]) {
+        // if self contains any partial block, hash it using cSHAKE128
         if let Some(partial) = self.has_partial_block.take() {
             let mut temp_out = [0u8; 32];
             let mut reader = partial.state.finalize_xof();
